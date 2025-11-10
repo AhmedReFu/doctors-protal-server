993d468a9f66cd18535cbd2f61c810400c478a8a
MD NAZMUL ISLAM <59439125+AhmedReFu@users.noreply.github.com>
2025-11-11 02:39:32 +0600

    Configure CORS to allow all origins with credentials

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 5001;
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhksq.mongodb.net/?retryWrites=true&w=majority`;
